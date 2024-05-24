6babddac20c7b22385c34fff3e00a9104277df19
VivekR1990 <142075803+VivekR1990@users.noreply.github.com>
2024-05-24 10:27:34 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -52,12 +52,5 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-      apiKey: "AIzaSyBb0uHuV0SB5CcuzR7kX7AM8sv5erkW1sY",
-      authDomain: "student-logbook-58249.firebaseapp.com",
-      projectId: "student-logbook-58249",
-      storageBucket: "student-logbook-58249.appspot.com",
-      messagingSenderId: "1059158103429",
-      appId: "1:1059158103429:web:144229435d2d49c90560ec",
-      measurementId: "G-B3487MNE3J");
+  
 }
